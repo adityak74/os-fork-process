90682fa0e202efa23e34fde2d39c5dd88ad8bcd2
Aditya Karnam <akarnam37@gmail.com>
2017-09-08 01:02:57 -0500

    Added getopt and Usage

--- README.md
+++ README.md
@@ -24,6 +24,16 @@ Running the project
 $ ./forkos -n3 -k4 -m10
 ```
 
+Help:
+
+```
+$ ./forkos -h
+Usage : ./forkos -n <int> -k <int> -m <int>
+n = number of process to fork
+k = number of times to loop on fprintf
+m = time to sleep in millis
+```
+
 Clean the project:
 
 ```
--- src/os_fork.c
+++ src/os_fork.c
@@ -1,18 +1,55 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <ctype.h>
 int main (int argc, char *argv[]) {
    pid_t childpid = 0;
-   int i, n;
-   if (argc != 2){   /* check for valid number of command-line arguments */
-      fprintf(stderr, "Usage: %s processes\n", argv[0]);
-      return 1;
+   int i, n, k, m, index;
+   int c;
+
+   opterr = 0;
+   
+   if ( argc < 2 ) {
+   		fprintf(stderr, "Usage : %s -n <int> -k <int> -m <int>\n", argv[0] );
+	    return 1;
    }
-   n = atoi(argv[1]);
-   for (i = 1; i < n; i++)
-      if (childpid = fork())
-         break;
-   fprintf(stderr, "i:%d  process ID:%ld  parent ID:%ld  child ID:%ld\n",
-           i, (long)getpid(), (long)getppid(), (long)childpid);
+
+   while((c = getopt (argc, argv, "h::k:m:n:")) != -1)
+    switch (c)
+    {
+      case 'n':
+          n = atoi(optarg);
+          break;
+	  case 'k':
+	      k = atoi(optarg);
+	      break;
+	  case 'm':
+	      m = atoi(optarg);
+	      break;
+	  case 'h':
+	      fprintf(stderr, "Usage : %s -n <int> -k <int> -m <int>\nn = number of process to fork\nk = number of times to loop\nm = time to sleep in millis\n\n", argv[0] );
+	      return 1;
+	  case '?':
+	      fprintf(stderr, "Try `%s -h' for more information.\n", argv[0]);
+		  return -1;
+	  default:
+	      fprintf(stderr, "Usage: %s -n <int> -k <int> -m <int>\n", argv[0]);
+	      abort();
+	  }
+	  
+	  if(argc != 4){
+	  	fprintf(stderr, "Try `%s -h' for more information.\n", argv[0]);
+	  	return -1;
+	  }
+	  
+	  for (index = optind; index < argc; index++)
+    	printf ("Non-option argument %s\n", argv[index]);	   
+
+   // for (i = 1; i < n; i++)
+//       if (childpid = fork())
+//          break;
+   
+//    fprintf(stderr, "i:%d  process ID:%ld  parent ID:%ld  child ID:%ld\n",
+//            i, (long)getpid(), (long)getppid(), (long)childpid);
    return 0;
 }
